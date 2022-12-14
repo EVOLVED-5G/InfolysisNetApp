494ce1e5891bdb3c741a2dd112130e07fd5c4f73
Christos Sakkas <chsakkas@hotmail.com>
2022-12-14 16:58:05 +0200

    Working version with CAPIF integration

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM pensiero/apache-php-mysql
 
 RUN apt-get update && apt-get install -y software-properties-common
 RUN add-apt-repository ppa:deadsnakes/ppa
-RUN apt-get update && apt-get install -y net-tools iputils-ping vim unzip tcpdump bmon mysql-server python3.9 python3.9-distutils python3-setuptools
+RUN apt-get update && apt-get install -y net-tools iputils-ping vim unzip tcpdump bmon mysql-server python3.9 python3.9-distutils python3-setuptools jq
 RUN curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 RUN python3.9 get-pip.py
 
@@ -14,7 +14,12 @@ COPY entrypoint3.py /var/www/
 COPY 000-default.conf /etc/apache2/sites-available/000-default.conf
 COPY netappcontroller /var/www/
 COPY netapp_docker_test_deployment.py /var/www/
+COPY dummy /var/www/
 ENV NEF_HOSTNAME="http://185.184.71.39:8888"
+ENV CAPIF_HOSTNAME=capifcore
+ENV CAPIF_PORT_HTTP=8080
+ENV CAPIF_PORT_HTTPS=443
+
 #COPY rc.local /etc/
 #RUN chmod 755 /etc/rc.local
 
--- entrypoint.py
+++ entrypoint.py
@@ -9,13 +9,14 @@ os.system("mysql < /var/www/html/configui/nettapp_evolved5g.sql");
 #os.system("mysql -e \"insert into admins values (2,'admin2','315f166c5aca63a157f7d41007675cb44a948b33','admin','','1',null)\"");
 os.system("mysql -e \"ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root'\"");
 os.system("pip3.9 install uvicorn")
-os.system("pip3.9 install fastapi")
+os.system("pip3.9 install fastapi==0.70.0")
 os.system("pip3.9 install sqlalchemy")
 os.system("pip3.9 install pymysql")
 os.system("pip3.9 install --upgrade requests==2.20.1")
 os.system("pip3.9 install fastapi")
 os.system("pip3.9 install --upgrade setuptools")
-os.system("pip3.9 install --upgrade evolved5g==0.6.8")
-os.system("pip install typing-extensions --upgrade")
+os.system("pip3.9 install --upgrade evolved5g")
+os.system("pip3.9 install typing-extensions==3.10.0.2")
+os.system("pip3.9 install pydantic==1.8.2")
 
 #os.system("sh /var/www/arxi.sh")
