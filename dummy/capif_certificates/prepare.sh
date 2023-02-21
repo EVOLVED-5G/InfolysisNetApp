ebdbc98f405e352b8a262a3d10dce97f2829b4eb
Christos Sakkas <chsakkas@hotmail.com>
2023-02-21 12:58:32 +0200

    Fixed duplicate enviromental variables in capif_registration.json

--- dummy/capif_certificates/prepare.sh
+++ dummy/capif_certificates/prepare.sh
@@ -1,7 +1,7 @@
 
 jq -r .capif_host=\"$CAPIF_HOSTNAME\" /var/www/capif_certificates/capif_registration.json >> tmp.json && mv tmp.json /var/www/capif_certificates/capif_registration.json
-jq -r .capif_port_http=\"$CAPIF_PORT_HTTP\" /var/www/capif_certificates/capif_registration.json >> tmp.json && mv tmp.json /var/www/capif_certificates/capif_registration.json
-jq -r .capif_port_https=\"$CAPIF_PORT_HTTPS\" /var/www/capif_certificates/capif_registration.json >> tmp.json && mv tmp.json /var/www/capif_certificates/capif_registration.json
+jq -r .capif_http_port=\"$CAPIF_PORT_HTTP\" /var/www/capif_certificates/capif_registration.json >> tmp.json && mv tmp.json /var/www/capif_certificates/capif_registration.json
+jq -r .capif_https_port=\"$CAPIF_PORT_HTTPS\" /var/www/capif_certificates/capif_registration.json >> tmp.json && mv tmp.json /var/www/capif_certificates/capif_registration.json
 
 
 evolved5g register-and-onboard-to-capif --config_file_full_path="/var/www/capif_certificates/capif_registration.json"
