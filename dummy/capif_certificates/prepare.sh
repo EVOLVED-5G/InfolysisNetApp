a4c7bdb806fe56fbf390b2e7616ee2652117ad58
confragkos <cfragos@infolysis.gr>
2023-07-14 13:51:42 +0300

    Add ENVIRONMENT_MODE as environment variable

--- dummy/capif_certificates/prepare.sh
+++ dummy/capif_certificates/prepare.sh
@@ -4,6 +4,6 @@ jq -r .capif_http_port=\"$CAPIF_PORT_HTTP\" /var/www/capif_certificates/capif_re
 jq -r .capif_https_port=\"$CAPIF_PORT_HTTPS\" /var/www/capif_certificates/capif_registration.json >> tmp.json && mv tmp.json /var/www/capif_certificates/capif_registration.json
 
 
-evolved5g register-and-onboard-to-capif --config_file_full_path="/var/www/capif_certificates/capif_registration.json" --environment="development"
+evolved5g register-and-onboard-to-capif --config_file_full_path="/var/www/capif_certificates/capif_registration.json" --environment="$ENVIRONMENT_MODE"
 
 #tail -f /dev/null
